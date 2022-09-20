b2281a8cda55f3a64fd0815b24ea9069b73c83f6
ysernah <43530180+ysernah@users.noreply.github.com>
2022-09-20 12:15:18 -0500

    Add files via upload

--- src/controllers/whatsappControllers.js
+++ src/controllers/whatsappControllers.js
@@ -7,8 +7,8 @@ const VerifyToken =(req, res) =>{
             var token =req.query["hub.verify_token"];
             var challenge = req.query["hub.challenge"];
 
-            if(challenge!=null && token!=null && token ==accessToken){
-                res.send(challenge)
+            if(challenge!=null && token != null && token == accessToken){
+                res.send(challenge);
             }else{
                  res.status(400).send();
                 }
