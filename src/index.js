a12871e9716e29b1b43df095772ab653fe1a4620
ysernah <43530180+ysernah@users.noreply.github.com>
2022-10-01 16:31:17 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,9 +9,5 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.use("/whatsapp",apiRoute);
-app.get('/',(req,res)=>{
-    res.render('app')
-})
-
 app.listen(PORT, () =>{console.log("el puerto es :"+ PORT)});
 
